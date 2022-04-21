8b8c94a815efe8eb0ae208fc5233b2d7b13c9110
Assassin Duck <88781785+TheSadError@users.noreply.github.com>
2022-04-21 17:33:23 +0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,11 @@
 import os
-from colorama import Fore,Back,Style
-
-class bcolors:
-    OK = '\033[92m' 
-    WARNING = '\033[93m' 
-    FAIL = '\033[91m' 
-    RESET = '\033[0m' 
-
-print(f"{bcolors.FAIL}[1]      Debian Based (Kali,Parrot,KDE...)")
-print(f"{bcolors.FAIL}[2]      Arch Based (Arch,Manjora,GARUDA...)")
-n = int(input(f"{bcolors.FAIL}[OS] Select Your Os : "))
+print("[1]      Debian Based (Kali,Parrot,KDE...)")
+print("[2]      Arch Based (Arch,Manjora,GARUDA...)")
+n = int(input("[OS] Select Your Os : "))
 
 if n==1:
     os.system("sudo bash ./nivo/debian.sh")
 elif n==2:
     os.system("sudo bash ./nivo/arch.sh")
 else:
-    print(f"{bcolors.WARNING}[WARNING] Invalid Command Dedected. Please Input Valid Commands.")
+    print("[WARNING] Invalid Command Dedected. Please Input Valid Commands.")
