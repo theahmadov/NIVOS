c5f61ea00d95bf5461b447417257b83f2d2474e9
Assassin Duck <88781785+TheSadError@users.noreply.github.com>
2022-04-21 17:17:28 +0400

    Update main.py

--- main.py
+++ main.py
@@ -17,10 +17,11 @@ print(f"{bcolors.OK}[NIVOS] Welcome To NIVO IP Scanner -NIVOS-")
 print(" ")
 print(f"{bcolors.OK}[INFO] You Must Run This Tool By Super User Power.")
 print(" ")
-print(f"{bcolors.FAIL}[1]     Scan Devices In Your Network")
-print(f"{bcolors.FAIL}[2]     Scan Networks")
-print(f"{bcolors.FAIL}[3]     Try Crack Network With Mac Adress")
-print(f"{bcolors.FAIL}[4]     Scan Website")
+print(f"{bcolors.WARNING}[1]     Scan Devices In Your Network")
+print(f"{bcolors.WARNING}[2]     Scan Networks")
+print(f"{bcolors.WARNING}[3]     Try Crack Network With Mac Adress")
+print(f"{bcolors.WARNING}[4]     Scan Website")
+print(f"{bcolors.WARNING}[5]     DOS-DDOS")
 print(" ")
 i = int(input(f"{bcolors.OK}[MENU] Please Select Operation Number : "))
 if i==1:
@@ -31,6 +32,8 @@ elif i == 3:
     os.system("sudo python3 ./nivo/NIVO_CRACK.py")
 elif i == 4:
     os.system("sudo python3 ./nivo/NIVO_WEB.py")
+elif i == 5:
+    os.system("sudo python3 ./nivo/NIVO_DOS.py")
 
 else:
     print(f"{bcolors.WARNING}[WARNING] Invalid Command Dedected. Please Input Valid Commands.")
