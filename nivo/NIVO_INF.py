ef0b9886d2b4b56bbe1212d098b66a7dab8d1455
Assassin Duck <88781785+TheSadError@users.noreply.github.com>
2022-04-22 15:29:33 +0400

    Create NIVO_INF.py

---
+++ nivo/NIVO_INF.py
@@ -0,0 +1,17 @@
+import platform
+from colorama import Fore,Back,Style
+import os
+import socket
+class bcolors:
+    OK = '\033[92m' 
+    WARNING = '\033[93m' 
+    FAIL = '\033[91m' 
+    RESET = '\033[0m' 
+my_system = platform.uname()
+
+print(f"{bcolors.OK}[INFO] System      : {my_system.system}")
+print(f"{bcolors.OK}[INFO] Node Name   : {my_system.node}")
+print(f"{bcolors.OK}[INFO] Release     : {my_system.release}")
+print(f"{bcolors.OK}[INFO] Version     : {my_system.version}")
+print(f"{bcolors.OK}[INFO] Machine     : {my_system.machine}")
+print(f"{bcolors.OK}[INFO] Processor   : {my_system.processor}")
