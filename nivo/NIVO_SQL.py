d412488496074ed4b43f1fa46bb2db0dc58def91
NIVOS Developer <88781785+TheSadError@users.noreply.github.com>
2022-04-25 12:48:33 +0400

    Update NIVO_SQL.py

--- nivo/NIVO_SQL.py
+++ nivo/NIVO_SQL.py
@@ -1,60 +1,96 @@
+import requests
+from bs4 import BeautifulSoup as bs
+from urllib.parse import urljoin
+from pprint import pprint
 import os
 import time
 from colorama import Fore,Back,Style
 import socket
 class bcolors:
     OK = '\033[92m' 
-    a = '\033[93m' 
+    WARNING = '\033[93m' 
     FAIL = '\033[91m' 
     RESET = '\033[0m' 
 os.system("clear")
-os.system("figlet Error | lolcat")
-print(f"{bcolors.FAIL}This Tool Created By Error")
-print(f"{bcolors.FAIL}Github  : https://github.com/TheSadError ")
-print(f"{bcolors.FAIL}Discord : err0r#4018")
-print(" ")
-print(f"{bcolors.OK}[NIVOS] Welcome To NIVOS -NIVOS-")
-print(" ")
-print(f"{bcolors.OK}[INFO] You Must Run This Tool By Super User Power.")
-print(" ")
-print(f"{bcolors.FAIL}[1]     Scan Devices In Your Network")
-print(f"{bcolors.FAIL}[2]     Scan Networks")
-print(f"{bcolors.FAIL}[3]     Try Crack Network With Mac [Adress Aircrack-ng]")
-print(f"{bcolors.FAIL}[4]     Scan Website [NMAP, WHOIS]")
-print(f"{bcolors.FAIL}[5]     DOS-DDOS")
-print(f"{bcolors.FAIL}[6]     Phone [Information Gathering , SMS Sender]")
-print(f"{bcolors.FAIL}[7]     Get Your Information [System INFO]")
-print(f"{bcolors.FAIL}[8]     Sniffing , Spoofing [Bettercap]")
-print(f"{bcolors.FAIL}[9]     IP [Information Gathering]")
-print(f"{bcolors.FAIL}[10]    Verify Gmail")
-print(f"{bcolors.FAIL}[11]    Port Scanner")
-print(f"{bcolors.FAIL}[12]    SQL Scan (WEBSITE)")
-print(" ")
-i = int(input(f"{bcolors.OK}[MENU] Please Select Operation Number : "))
-if i==1:
-    os.system("sudo python3 ./nivo/NIVO_IP.py")
-elif i==2:
-    os.system("sudo python3 ./nivo/NIVO_NET.py")
-elif i == 3:
-    os.system("sudo python3 ./nivo/NIVO_CRACK.py")
-elif i == 4:
-    os.system("sudo python3 ./nivo/NIVO_WEB.py")
-elif i == 5:
-    os.system("sudo python3 ./nivo/NIVO_DOS.py")
-elif i == 6:
-    os.system("sudo python3 ./nivo/NIVO_PH.py")
-elif i == 7:
-    os.system("sudo python3 ./nivo/NIVO_INF.py")
-elif i == 8:
-    os.system("sudo python3 ./nivo/NIVO_BET.py")
-elif i == 9:
-    os.system("sudo python3 ./nivo/NIVO_IPF.py")
-elif i == 10:
-    os.system("sudo python3 ./nivo/NIVO_EM.py")
-elif i == 11:
-    os.system("sudo python3 ./nivo/NIVO_PRT.py")
-elif i == 12:
-    os.system("sudo python3 ./nivo/NIVO_SQL.py")
+print(f"{bcolors.FAIL}[NIVOS] Welcome To NIVOS SQL Injection Tool : ")
+s = requests.Session()
+s.headers["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.106 Safari/537.36"
 
-else:
-    print(f"{bcolors.FAIL}[FAIL] Invalid Command Dedected. Please Input Valid Commands.")
+def get_all_forms(url):
+    """Given a `url`, it returns all forms from the HTML content"""
+    soup = bs(s.get(url).content, "html.parser")
+    return soup.find_all("form")
+
+
+def get_form_details(form):
+    """
+    This function extracts all possible useful information about an HTML `form`
+    """
+    details = {}
+    try:
+        action = form.attrs.get("action").lower()
+    except:
+        action = None
+    method = form.attrs.get("method", "get").lower()
+    inputs = []
+    for input_tag in form.find_all("input"):
+        input_type = input_tag.attrs.get("type", "text")
+        input_name = input_tag.attrs.get("name")
+        input_value = input_tag.attrs.get("value", "")
+        inputs.append({"type": input_type, "name": input_name, "value": input_value})
+    details["action"] = action
+    details["method"] = method
+    details["inputs"] = inputs
+    return details
+
+
+def is_vulnerable(response):
+    """A simple boolean function that determines whether a page 
+    is SQL Injection vulnerable from its `response`"""
+    errors = {
+        "you have an error in your sql syntax;",
+        "warning: mysql",
+        "unclosed quotation mark after the character string",
+        "quoted string not properly terminated",
+    }
+    for error in errors:
+        if error in response.content.decode().lower():
+            return True
+    return False
+
+def scan_sql_injection(url):
+    for c in "\"'":
+        new_url = f"{url}{c}"
+        print("[!] Trying", new_url)
+        res = s.get(new_url)
+        if is_vulnerable(res):
+            print("[+] SQL Injection vulnerability detected, link:", new_url)
+            return
+    forms = get_all_forms(url)
+    print(f"[+] Detected {len(forms)} forms on {url}.")
+    for form in forms:
+        form_details = get_form_details(form)
+        for c in "\"'":
+            data = {}
+            for input_tag in form_details["inputs"]:
+                if input_tag["type"] == "hidden" or input_tag["value"]:
+                    try:
+                        data[input_tag["name"]] = input_tag["value"] + c
+                    except:
+                        pass
+                elif input_tag["type"] != "submit":
+                    data[input_tag["name"]] = f"test{c}"
+            url = urljoin(url, form_details["action"])
+            if form_details["method"] == "post":
+                res = s.post(url, data=data)
+            elif form_details["method"] == "get":
+                res = s.get(url, params=data)
+            if is_vulnerable(res):
+                print("[+] SQL Injection vulnerability detected, link:", url)
+                print("[+] Form:")
+                pprint(form_details)
+                break
+if __name__ == "__main__":
+    inurl = input(f"{bcolors.WARNING}[NIVOS] Please Input URL : ")
+    url = inurl
+    scan_sql_injection(url)
