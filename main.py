b54d545ba88f9554da18cd79ff14f033838b862e
NIVOS Developer <88781785+TheSadError@users.noreply.github.com>
2022-04-25 16:31:00 +0400

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ class bcolors:
     a = '\033[93m' 
     FAIL = '\033[91m' 
     RESET = '\033[0m' 
+    #TheSadError Repo
 os.system("clear")
 os.system("figlet Error | lolcat")
 print(f"{bcolors.FAIL}This Tool Created By Error")
@@ -17,44 +18,44 @@ print(f"{bcolors.OK}[NIVOS] Welcome To NIVOS -NIVOS-")
 print(" ")
 print(f"{bcolors.OK}[INFO] You Must Run This Tool By Super User Power.")
 print(" ")
-print(f"{bcolors.FAIL}[1]     Scan Devices In Your Network")
-print(f"{bcolors.FAIL}[2]     Scan Networks")
-print(f"{bcolors.FAIL}[3]     Try Crack Network With Mac [Adress Aircrack-ng]")
-print(f"{bcolors.FAIL}[4]     Scan Website [NMAP, WHOIS]")
-print(f"{bcolors.FAIL}[5]     DOS-DDOS")
-print(f"{bcolors.FAIL}[6]     Phone [Information Gathering , SMS Sender]")
-print(f"{bcolors.FAIL}[7]     Get Your Information [System INFO]")
-print(f"{bcolors.FAIL}[8]     Sniffing , Spoofing [Bettercap]")
-print(f"{bcolors.FAIL}[9]     IP [Information Gathering]")
-print(f"{bcolors.FAIL}[10]    Verify Gmail")
-print(f"{bcolors.FAIL}[11]    Port Scanner")
-print(f"{bcolors.FAIL}[12]    SQL Scan (WEBSITE)")
+print(f"{bcolors.FAIL}[1]     Scan Devices In Your Network") # Error
+print(f"{bcolors.FAIL}[2]     Scan Networks") # Error
+print(f"{bcolors.FAIL}[3]     Try Crack Network With Mac [Adress Aircrack-ng]") # Error
+print(f"{bcolors.FAIL}[4]     Scan Website [NMAP, WHOIS]") # Error
+print(f"{bcolors.FAIL}[5]     DOS-DDOS") # Error
+print(f"{bcolors.FAIL}[6]     Phone [Information Gathering , SMS Sender]") # Error
+print(f"{bcolors.FAIL}[7]     Get Your Information [System INFO]") # Error
+print(f"{bcolors.FAIL}[8]     Sniffing , Spoofing [Bettercap]") # Error
+print(f"{bcolors.FAIL}[9]     IP [Information Gathering]") # Error
+print(f"{bcolors.FAIL}[10]    Verify Gmail") # Error
+print(f"{bcolors.FAIL}[11]    Port Scanner") # Error
+print(f"{bcolors.FAIL}[12]    SQL Scan (WEBSITE)") # Error
 print(" ")
-i = int(input(f"{bcolors.OK}[MENU] Please Select Operation Number : "))
+i = int(input(f"{bcolors.OK}[MENU] Please Select Operation Number : ")) # Error
 if i==1:
-    os.system("sudo python3 ./nivo/NIVO_IP.py")
+    os.system("sudo python3 ./nivo/NIVO_IP.py") # Error
 elif i==2:
-    os.system("sudo python3 ./nivo/NIVO_NET.py")
+    os.system("sudo python3 ./nivo/NIVO_NET.py") # Error
 elif i == 3:
-    os.system("sudo python3 ./nivo/NIVO_CRACK.py")
+    os.system("sudo python3 ./nivo/NIVO_CRACK.py") # Error
 elif i == 4:
-    os.system("sudo python3 ./nivo/NIVO_WEB.py")
+    os.system("sudo python3 ./nivo/NIVO_WEB.py") # Error
 elif i == 5:
-    os.system("sudo python3 ./nivo/NIVO_DOS.py")
+    os.system("sudo python3 ./nivo/NIVO_DOS.py") # Error
 elif i == 6:
-    os.system("sudo python3 ./nivo/NIVO_PH.py")
+    os.system("sudo python3 ./nivo/NIVO_PH.py") # Error
 elif i == 7:
-    os.system("sudo python3 ./nivo/NIVO_INF.py")
+    os.system("sudo python3 ./nivo/NIVO_INF.py") # Error
 elif i == 8:
-    os.system("sudo python3 ./nivo/NIVO_BET.py")
+    os.system("sudo python3 ./nivo/NIVO_BET.py") # Error
 elif i == 9:
-    os.system("sudo python3 ./nivo/NIVO_IPF.py")
+    os.system("sudo python3 ./nivo/NIVO_IPF.py") # Error
 elif i == 10:
-    os.system("sudo python3 ./nivo/NIVO_EM.py")
+    os.system("sudo python3 ./nivo/NIVO_EM.py") # Error
 elif i == 11:
-    os.system("sudo python3 ./nivo/NIVO_PRT.py")
+    os.system("sudo python3 ./nivo/NIVO_PRT.py") # Error
 elif i == 12:
-    os.system("sudo python3 ./nivo/NIVO_SQL.py")
+    os.system("sudo python3 ./nivo/NIVO_SQL.py") # Error
 
 else:
-    print(f"{bcolors.FAIL}[FAIL] Invalid Command Dedected. Please Input Valid Commands.")
+    print(f"{bcolors.FAIL}[FAIL] Invalid Command Dedected. Please Input Valid Commands.") # Error
